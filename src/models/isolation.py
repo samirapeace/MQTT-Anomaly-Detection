9bc31533c7530bcda8b3f2f88ec26a670727b2e4
samirapeace <almuftisalam@gmail.com>
2026-05-05 22:30:20 +0300

    Update isolation.py

--- src/models/isolation.py
+++ src/models/isolation.py
@@ -15,39 +15,28 @@ class IsolationModel:
         self.scaler = StandardScaler()
         self.epsilon = 1e-9
 
-    # =========================
-    # TRAIN
-    # =========================
+
     def train(self, df, label_column="label"):
 
-        # فقط benign
         benign_df = df[df[label_column] == 3]
         X = benign_df.drop(columns=[label_column], errors="ignore")
 
-        # 🔥 scaling
         X_scaled = self.scaler.fit_transform(X)
 
         self.model.fit(X_scaled)
 
-    # =========================
-    # COMPUTE SCORES
-    # =========================
+
     def compute_scores(self, df, label_column="label"):
 
         X = df.drop(columns=[label_column], errors="ignore")
 
-        # نفس scaling
         X_scaled = self.scaler.transform(X)
 
-        # anomaly score
         raw_scores = self.model.decision_function(X_scaled)
 
-        # =========================
-        # تحويل لـ anomaly probability-like 🔥
-        # =========================
-        scores = -raw_scores  # inversion (higher = more anomalous)
 
-        # normalization 0–1
+        scores = -raw_scores  
+   
         scores = (scores - scores.min()) / (
             scores.max() - scores.min() + self.epsilon
         )
